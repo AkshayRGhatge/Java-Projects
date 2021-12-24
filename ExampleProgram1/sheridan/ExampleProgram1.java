67380f1ab2f4573b709e0e097b400f4acfbba597
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-24 11:59:29 -0500

    Adding Ex1

---
+++ ExampleProgram1/sheridan/ExampleProgram1.java
@@ -0,0 +1,24 @@
+/*ExampleProgram1.java : shows is a simple java application that multiplies number by two
+* author:Akshay Ghatge
+*/
+package sheridan;
+// Definition of the class ExampleProgram1
+public class ExampleProgram1
+{
+   public static void main(String[] args)
+   {
+       //Declaration and initailizations
+       int number;// represents the whole number
+       int result;// represent the double of the number
+       
+       number=12;//initialize number to 12
+       
+       // calculate two times number
+       result= number * 2;
+       
+       System.out.println("Result is " + result);
+       
+       
+   }//end of the main method
+    
+}// end of the class ExampleProgram1
