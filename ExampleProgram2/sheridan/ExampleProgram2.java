07b8c1f4fb3848f0b6147278b1d28451d9f502b7
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-24 11:59:46 -0500

    Adding Ex2

---
+++ ExampleProgram2/sheridan/ExampleProgram2.java
@@ -0,0 +1,19 @@
+/*ExampleProgram2.java is the program with the four lines output.
+ * author:Akshay Ghatge
+ */
+
+package sheridan;
+
+//Definition of the class ExampleProgram2
+public class ExampleProgram2{
+    
+public static void main(String[] args)
+        {
+    System.out.println("Hello PROG10082");
+    System.out.println("\tThis is \"Java\" programming");    
+    System.out.println("           Is today Friday?(Y\\N)");
+    System.out.println("               Java is a \'3\' rd generation");
+    
+    }//end of the main method
+      
+}// end of the ExampleProgram2.java
